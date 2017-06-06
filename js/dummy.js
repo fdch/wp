3ab22d553871ad9067d1c51edc05816f03f77505
Fede Camara Halac <camarafede@gmail.com>
2017-06-06 17:49:57 -0400

    updating event-form

--- js/dummy.js
+++ js/dummy.js
@@ -2,21 +2,23 @@ $(function(){
   var w = $(window).width();
   var h = $(window).height();
   if (w >= 600) w = w*0.5;
-  var thePrompt = window.open("", "", "width="+w/2+", height="+h/2);
+  var thePrompt = window.open("", "", "width=300,height=200");
   var theHTML = "";
 
   theHTML += "<h4>User</h4>: <input type=text id=mrkfir />";
   theHTML += "<h4>Pass</h4>: <input type=text id=mrklas />";
+  theHTML += "<br/>";
   theHTML += "<input type=button value=OK id=authOK />";
   thePrompt.document.body.innerHTML = theHTML;
 
   var mrkfir = thePrompt.document.getElementById("mrkfir").value;
   var mrklas = thePrompt.document.getElementById("mrklas").value;
   thePrompt.document.getElementById("authOK").onclick = function () {
-    if(mrkfir === 'karlheinz' && mrklas === 'stockhausen') {
-    thePrompt.close();
-    $("body").append("<iframe src='https://docs.google.com/forms/d/e/1FAIpQLSdWV-2zEgbjF6WDroZrZx-bAqoXG8Tx3v_0XwA1dwhJIBafUA/viewform?embedded=true' width=" + w + " height=" + h + " frameborder=0 marginheight=0 marginwidth=0>Loading...</iframe>");
-    } else
-      window.alert("Try Again");
+    if(mrkfir !== 'karlheinz' && mrklas !== 'stockhausen') {
+    window.alert("Try Again");
+    } else {
+      thePrompt.close();
+      $("body").append("<iframe src='https://docs.google.com/forms/d/e/1FAIpQLSdWV-2zEgbjF6WDroZrZx-bAqoXG8Tx3v_0XwA1dwhJIBafUA/viewform?embedded=true' width=" + w + " height=" + h + " frameborder=0 marginheight=0 marginwidth=0>Loading...</iframe>");
+    }
 }
 });
