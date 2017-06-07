da0ff7ccea57133baa7ef1710bcfbd4d4f7fafe2
Fede Camara Halac <camarafede@gmail.com>
2017-06-07 15:18:21 -0400

    updates

---
+++ js/config.js
@@ -0,0 +1,19 @@
+////////////
+//// Below are some editable variables
+////////////
+var url = "https://waverlyproject.org";
+var email = "waverlyproject1@gmail.com";
+var facebookUrl = "https://www.facebook.com/waverlyproject/";
+var title = "Waverly Project";
+var subtitle = "New Music Force by NYU Composers";
+var logoimage = ["img/logo", 200, 200];
+var menu = ["About", "People", "Events", "Links", "Submit"];
+var eventsText = "Below are listed all events (past, present and future) hosted by Waverly Project. We invite you to this menu to navigate through all our seasons, and to click on any event to get more information.";
+var aboutOutro = "Please contact us at <a href='mailto:"+email+"'>"+email+"</a> or via our <a href='"+facebookUrl+"' target=_blank>Facebook Page </a>";
+var keywords = "waverly, project, music, festival, organization, nyu, nyc, new york university, proyecto, nueva musica, neue musik, contemporary, art, multimedia, ensemble, lectures, masterclasses, concerts, performers, performances, the soon another, talea, iktus percussion, mise-en, angelakis, camara, halac, oliver la rosa";
+////////////
+//// Meta properties
+////////////
+var fbappid = 123442671081764;
+var meta = "<meta property=og:image content='fdch.github.io/wp/"+logoimage[0]+"'/><meta name=keywords content='"+keywords+"' /><meta name=description content='" + url + "'<meta name=robots content='index, follow' /><meta name=author content='http://githug.io/fdch' /><meta property=fb:app_id content="+fbappid+"/><meta property=og:url content='" + url + "' /><meta property=og:title content='" + title + "' /><meta property=og:site_name content='" + keywords + "'/><title>'" + title + "' | '" + subtitle + "'</title>";
+var analytics = "<!--Google Analytics--><script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-61057132-1', 'auto');ga('send', 'pageview');</script><!--End Google Analytics-->"
--- js/main.js
+++ js/main.js
@@ -1,50 +1,21 @@
 ////////////
-//// Below are some editable strings that are too small to put outside.
-////////////
-var url = "https://waverlyproject.org";
-var email = "waverlyproject1@gmail.com";
-var facebookUrl = "https://www.facebook.com/waverlyproject/";
-var title = "Waverly Project";
-var subtitle = "New Music Force by NYU Composers";
-var logoimage = ["img/logo", 200, 200];
-var menu = ["About", "People", "Events", "Links", "Submit"];
-var eventsText = "Below are listed all events (past, present and future) hosted by Waverly Project. We invite you to this menu to navigate through all our seasons, and to click on any event to get more information.";
-var aboutOutro = "Please contact us at <a href='mailto:"+email+"'>"+email+"</a> or via our <a href='"+facebookUrl+"' target=_blank>Facebook Page </a>";
-var keywords = "waverly, project, music, festival, organization, nyu, nyc, new york university, proyecto, nueva musica, neue musik, contemporary, art, multimedia, ensemble, lectures, masterclasses, concerts, performers, performances, the soon another, talea, iktus percussion, mise-en, angelakis, camara, halac, oliver la rosa";
-////////////
-//// Meta properties
-////////////
-var fbappid = 123442671081764;
-var meta = "<meta property=og:image content='fdch.github.io/wp/"+logoimage[0]+"'/><meta name=keywords content='"+keywords+"' /><meta name=description content='" + url + "'<meta name=robots content='index, follow' /><meta name=author content='http://githug.io/fdch' /><meta property=fb:app_id content="+fbappid+"/><meta property=og:url content='" + url + "' /><meta property=og:title content='" + title + "' /><meta property=og:site_name content='" + keywords + "'/><title>'" + title + "' | '" + subtitle + "'</title>";
-var analytics = "<!--Google Analytics--><script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-61057132-1', 'auto');ga('send', 'pageview');</script><!--End Google Analytics-->"
-////////////
 //// From now on, please be extra careful when you edit
 ////////////
+var w, h; //window width height
 var titleData = "<h1 onclick=\"location.href='"+ url + "'\">"+ title +"</h1><h2 onclick=\"location.href='" + url + "'\">" + subtitle + "</h2><div id=logo><img src='" + logoimage[0] + "' width="+ logoimage[1] +" height="+ logoimage[2] +"/></div><nav id=menu></nav><div id=content></div>";
 ////////////
-//// Begin js load
+//// Loading glue
 ////////////
-$(document).ready(function() {
-$("head").append(meta);
-$("body")
-  .append(titleData)
-  .append(analytics);
-  
-var w = function(){ var w = $(window).width(); if (w >= 600) w = w*0.5;}
-var h = function(){$(window).height();}
-var m = $("#menu");
-var t = $("#content");
-var te = $("#event-load");
 function wpLoadEvent(x) {te.load("event/" + x);}
 function wpLoadId(x) {t.load(x.id);}
 function wpLoad(x) {t.load(x)}
-for (i = 0; i < menu.lenght; i++) {
-  m.append("<span class=menulink onClick=\"get"+menu[i]+"()\">"+menu[i]+"</span>
-}
+////////////
+//// Menu functions
+////////////
 function getEvents() {
   var events = "<article><h3>Events</h3><p>"+ eventsText + "</p><div id=event-menu></div><div id=event-load></div></article>"
   t.append(events);
-  $("#event-menu").load("event/menu");
+  te.load("event/menu");
 }
 function getPeople() {
   t.append("<article><h3>People</h3><div id=people-list></div></article>");
@@ -99,4 +70,19 @@ function wpEventForm() {
     }
   }
 }
+////////////
+//// Begin js load
+////////////
+$(document).ready(function() {
+  w = function(){ var w = $(window).width(); if (w >= 600) w = w*0.5;}
+  h = function(){$(window).height();}
+  var m = $("#menu");
+  var t = $("#content");
+  var te = $("#event-load");
+  $("head").append(meta);
+  $("body")
+  .append([titleData,analytics]);
+  for (i = 0, j = menu.lenght; i < j; i++) {
+  m.append("<span class=menulink onClick=\"get"+menu[i]+"()\">"+menu[i]+"</span>
+  }
 });//document.ready
