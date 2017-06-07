3ffd008193ebba647517f1103f26645d79b7095f
Fede Camara Halac <camarafede@gmail.com>
2017-06-07 15:48:55 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ $(document).ready(function(loadMenu) {
   var te = $("#event-load");
   $("head").append(meta);
   $("body").append([titleData,analytics]);
-  alert(w + " is window width");
+  alert(menu.join);
   getMenu();
 });
 ////////////
