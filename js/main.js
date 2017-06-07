341899e45bcf25032ddcfb504eaac3c271f263ee
Fede Camara Halac <camarafede@gmail.com>
2017-06-07 18:19:22 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //// Please be extra careful when you edit this file
 ////////////
 var w, h, t, te;
-var mitem = ["About", "People", "Events", "Links", "Submit"],
+var mitem = ["About", "People", "Events", "Links", "Submit"];
 var quotes = ["\"No, what is important is neither linearity or non-linearity, but the change, the degree of change from something that doesn't move to other events with different tempos in particular.\"", "\"I no longer limit myself.\""];
 var promptHTML = "<head><link rel=stylesheet href='../css/style.css'></style><link rel='shortcut icon' href='../img/favicon'></link><title>Waverly Project Event Form</title></head><body><h2>Waverly Project Event Form</h2><div><form><h3>Enter password:</h3><input type=password id=krl size=12/><p>Click Submit when done</p><input type=button value=Submit id=authOK /></form></div></body>";
 var titleData = "<h1 onclick=\"location.href='"+ url + "'\">"+ title +"</h1><h2 onclick=\"location.href='" + url + "'\">" + subtitle + "</h2><div id=logo><img src='" + logoimage[0] + "' width="+ logoimage[1] +" height="+ logoimage[2] +"/></div><div id=menu></div><div id=content></div>";
