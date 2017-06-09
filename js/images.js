7fd69b7bd010cd7f962f0a58013175eae490fe0d
Fede Camara Halac <camarafede@gmail.com>
2017-06-08 21:22:54 -0400

    making some test with async loading of img

---
+++ js/images.js
@@ -0,0 +1,13 @@
+$(function() {
+var myimages = [];
+var mysrc = [];
+$.get('updates/people-list', function(data){
+  lines = data.split("\n");
+  var i;
+  for (i = 0; i < lines.length; i++) {
+    myimages.push(lines[i]);
+    mysrc.push("img/people/"+lines[i]+".jpg");
+  }
+  alert(myimages.join + mysrc.join);
+});
+});
