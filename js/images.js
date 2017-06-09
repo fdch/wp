f29c78d4299b57f2abc402fa5973a501d309534b
Fede Camara Halac <camarafede@gmail.com>
2017-06-08 23:35:31 -0400

    updates

--- js/images.js
+++ js/images.js
@@ -8,14 +8,14 @@ var createImage = function(src, title) {
 
 // array of images
 var images = [];
-
+var imgpath = "../img/people/";
 $(function() {
 
 $.get('../updates/people-list', function(data){
   lines = data.split("\n");
   var i;
   for (i = 0; i < lines.length; i++) {
-    images.push(createImage("../img/people/"+lines[i]+".jpg", lines[i]));
+    images.push(createImage(imgpath+lines[i], lines[i]));
   }
 });
 console.log(images);
